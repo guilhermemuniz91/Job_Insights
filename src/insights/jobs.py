23b0ca22a6b7c7dc5bb7f12778f44774199185db
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-01-24 12:52:36 -0300

    Implementado o metodo Read, atendendo ao req2

--- src/insights/jobs.py
+++ src/insights/jobs.py
@@ -1,12 +1,33 @@
 from typing import List, Dict
+import csv
 
 
 class ProcessJobs:
     def __init__(self) -> None:
         self.jobs_list = list()
 
-    def read(self) -> List[Dict]:
-        pass
+    def read(self, path: str) -> List[Dict]:
+        """
+        Lê o arquivo CSV e armazena os dados no formato de
+        uma lista de dicionários.
+
+        Args:
+            path: O caminho para o arquivo CSV.
+
+        Returns:
+            A lista de dicionários contendo os dados do arquivo.
+        """
+
+        with open(path, "r") as f:
+            reader = csv.reader(f, delimiter=",")
+
+            headers = next(reader)
+
+            for row in reader:
+                job = {header: value for header, value in zip(headers, row)}
+                self.jobs_list.append(job)
+
+            return self.jobs_list
 
     def get_unique_job_types(self) -> List[str]:
         pass
