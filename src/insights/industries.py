20fd7bacedf3c634093d83d0852c2d9ede2babd2
Guilherme Muniz <guilherme.muniz91@gmail.com>
2024-01-24 13:44:21 -0300

    Implementado o metodo get_unique_industries, atentendo ao req5

--- src/insights/industries.py
+++ src/insights/industries.py
@@ -1,12 +1,24 @@
 from src.insights.jobs import ProcessJobs
 from typing import List
 
-# initial commit
-
 
 class ProcessIndustries(ProcessJobs):
     def __init__(self):
         super().__init__()
 
     def get_unique_industries(self) -> List[str]:
-        pass
+        """
+        Retorna uma lista de valores únicos presentes na coluna `industry`,
+        desconsiderando valores vazios.
+
+        Returns:
+            Uma lista de strings contendo tipos únicos de indústria.
+        """
+
+        industries = set()
+        for job in self.jobs_list:
+            industry = job["industry"]
+            if industry:
+                industries.add(industry)
+
+        return list(industries)
